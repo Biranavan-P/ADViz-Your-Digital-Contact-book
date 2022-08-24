bdf27e62eaf3c1e25d25f6eaeb4c3e30fac93153
Biranavan <Biri>
2022-08-24 21:42:09 +0200

    improved connections to server

--- myExpressServer.js
+++ myExpressServer.js
@@ -3,7 +3,6 @@ const app = express()
 const path = require("path")
 
 let userId = 4
-let current_user = null
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 app.use(express.static("public"))
@@ -11,12 +10,11 @@ app.use(express.static("public"))
 
 let admina = {username: "admina", password: "password", role:"admin"};
 let normalo = {username: "normalo", password: "password", role:"normal"};
-//TODO Justin fragen wie man weiß welche contacts zurückschicken soll
 
-let contact1_admina = { "ID":0,name: "Unknown", lastname:"User(A)", street:"Wilhelminenhofstraße 75A", zipcode: "10318", city :"Berlin", country:"Germany", phone: 353637437, dateOfBirth: "1990-06-04", isPublic: true,owner : "admina"}
-let contact2_admina = {"ID":1,name:"John", lastname:"Doe(A)", street:"Wilhelminenhofstraße 75A", zipcode:"12459",  city :"Berlin",  country:"Germany",phone: 6436377, dateOfBirth:"1990-04-07", isPublic:false ,owner : "admina"}
-let contact1_normalo = {"ID":2,name:"Dennis",lastname: "Doe(N)",street: "Straße des 17. Juni 135", zipcode:"10623",  city :"Berlin",  country:"Germany",phone: 353637437,dateOfBirth: "1990-06-04", isPublic:true,owner : "normalo"}
-let contact2_normalo = {"ID":3,name:"Piet", lastname:"Doe(N)", street:"Kaiserswerther Str. 16-18",zipcode: "14195 ",  city : "Berlin",  country:"Germany",phone: 88325652, dateOfBirth:"1990-06-04",isPublic:true,owner : "normalo"}
+let contact1_admina = { ID:0,name: "Unknown", lastname:"User(A)", street:"Wilhelminenhofstraße 75A", zipcode: "10318", city :"Berlin", country:"Germany", phone: 353637437, dateOfBirth: "1990-06-04", isPublic: true,owner : "admina"}
+let contact2_admina = {ID:1,name:"John", lastname:"Doe(A)", street:"Wilhelminenhofstraße 75A", zipcode:"12459",  city :"Berlin",  country:"Germany",phone: 6436377, dateOfBirth:"1990-04-07", isPublic:false ,owner : "admina"}
+let contact1_normalo = {ID:2,name:"Dennis",lastname: "Doe(N)",street: "Straße des 17. Juni 135", zipcode:"10623",  city :"Berlin",  country:"Germany",phone: 353637437,dateOfBirth: "1990-06-04", isPublic:true,owner : "normalo"}
+let contact2_normalo = {ID:3,name:"Piet", lastname:"Doe(N)", street:"Kaiserswerther Str. 16-18",zipcode: "14195 ",  city : "Berlin",  country:"Germany",phone: 88325652, dateOfBirth:"1990-06-04",isPublic:true,owner : "normalo"}
 
 
 let  users = [admina,normalo]
@@ -40,9 +38,17 @@ function addContactDB(contact){
 
 
 }
-function getContactDB(){
-
-    return contacts.filter(X=> X.owner === current_user|| X.isPublic)
+function getContactDB(current_user){
+    let owner =getUserDB(current_user)
+    if (owner === undefined){
+        return []
+    }
+    else if ( owner.role === "admin"){
+        return contacts
+    }
+    else {
+        return contacts.filter(X => X.owner === current_user || X.isPublic )
+    }
 }
 function updateContactDB(contact){
     let objIndex = contacts.findIndex((obj => obj.ID === contact.ID))
@@ -74,13 +80,13 @@ function deleteContactDB(user_id){
 function authenticate (req,res){
     let name = req.body["name"]
     let password = req.body["password"]
+
     let contact = getUserDB(name)
     if(contact != null && contact.password === password){
-        current_user = contact.username
-        res.send(JSON.stringify(contact))
+        res.status(200).json(contact)
     }
     else{
-        res.status(401).send("unauthorized")
+        res.status(401).send("Unauthorized")
     }
 }
 
@@ -94,35 +100,37 @@ function addContact(req,res){
     let country = req.body["Country"]
     let phone = req.body["Phone"]
     let dob = req.body["Date of birth"]
-    let public = req.body["Public Contact"]
+    let visibility = req.body["Public Contact"]
     let owner= req.body["Owner"]
-    if (!first_name  || !last_name  || !adress || !zip || !city|| !public || !owner){
+    if (!first_name  || !last_name  || !adress || !zip || !city|| !visibility || !owner){
         res.status(400).send("Bad Request")
+        return
     }
+    let isPublic = (visibility === 'true');
     let contact = {
-        "ID":userId,
-        "first name ": first_name,
-        "last name" : last_name,
-        "adress" : adress,
-        "zip":zip,
-        "city":city,
-        "country":country,
-        "phone":phone,
-        "dob":dob,
-        "isPublic":public,
-        "owner": owner
+        ID:userId,
+        name: first_name,
+        lastname:last_name,
+        street:adress,
+        zipcode: zip,
+        city :city,
+        country:country,
+        phone: phone,
+        dateOfBirth:dob ,
+        isPublic: isPublic,
+        owner : owner
     }
 
     addContactDB(contact)
-    res.header("Location",("/contacts/")+userId.toString())
-    res.sendStatus(200)
+    res.set("Location",("/contacts/")+userId.toString())
+    res.status(201).send()
     userId++
     
 
 }
 
 function getContact(req,res){
-    let contact_list = getContactDB()
+    let contact_list = getContactDB(req.body.current_user)
     res.send(contact_list)
 }
 
@@ -138,24 +146,29 @@ function update_contact(req,res){
     let country = req.body["Country"]
     let phone = req.body["Phone"]
     let dob = req.body["Date of birth"]
-    let public = req.body["Public Contact"]
-    if (first_name === null || last_name === null || adress ===null || zip===null || city === null || public === null){
-        res.sendStatus(200)
+    let visibility = req.body["Public Contact"]
+    let owner= req.body["Owner"]
+    if (!first_name  || !last_name  || !adress || !zip || !city|| !visibility || !owner){
+        res.status(400).send("Bad Request")
+        return
     }
+    let isPublic = (visibility === 'true');
+
     let contact = {
-        "ID":userId,
-        "first name ": first_name,
-        "last name" : last_name,
-        "adress" : adress,
-        "zip":zip,
-        "city":city,
-        "country":country,
-        "phone":phone,
-        "dob":dob,
-        "isPublic":public
+        ID:userId,
+        name: first_name,
+        lastname:last_name,
+        street:adress,
+        zipcode: zip,
+        city :city,
+        country:country,
+        phone: phone,
+        dateOfBirth:dob ,
+        isPublic: isPublic,
+        owner : owner
     }
     if (updateContactDB(contact)){
-        res.sendStatus(200)
+        res.sendStatus(204)
 
     }
     else{

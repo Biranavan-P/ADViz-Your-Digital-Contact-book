35bb227e5b72b20fc2e408faddeb896593d49fad
Biranavan <Biri>
2022-09-08 13:13:36 +0200

    added clearContacts function to ensure same entrys everytime

--- test/myExpressServerTest.js
+++ test/myExpressServerTest.js
@@ -1,11 +1,78 @@
 const supertest = require("supertest")
 var expect = require('chai').expect
 const request = supertest("http://localhost:3000/");
+let clearDB = async function (){
+    const uri = "mongodb://localhost:27017"
+
+    const {MongoClient} = require('mongodb');
+    const client = new MongoClient(uri);
+
+    const dbName = "advizDB";
+    async function connectDB() {
+        try {
+            // Connect to the MongoDB cluster
+            await client.connect();
+        } catch (e) {
+            console.error(e);
+            await client.close();
+        }
+
+    }
+    async function doesCollectionExistInDb() {
+        const collections = await client.db(dbName).collections();
+        return collections.some(
+            (collection) => collection.collectionName === "contacts"
+        );
+    }
+
+
+
+    let contact1_admina = { _id:0,name: "Unknown", lastname:"User(A)", street:"Wilhelminenhofstraße 75A", zipcode: "10318",
+        city :"Berlin", country:"Germany", phone: 353637437, dateOfBirth: "1990-06-04", isPublic: true,owner : "admina",
+        lat:52.49326,lng:13.52614
+    }
+    let contact2_admina = {_id:1,name:"John", lastname:"Doe(A)", street:"Wilhelminenhofstraße 75A", zipcode:"12459",
+        city :"Berlin",  country:"Germany",phone: 6436377, dateOfBirth:"1990-04-07", isPublic:false ,owner : "admina",
+        lat:52.4581,lng:13.52709
+    }
+    let contact1_normalo = {_id:2,name:"Dennis",lastname: "Doe(N)",street: "Straße des 17. Juni 135", zipcode:"10623",
+        city :"Berlin",  country:"Germany",phone: 353637437,dateOfBirth: "1990-06-04", isPublic:true,owner : "normalo",
+        lat:52.51274,lng:13.3269
+    }
+    let contact2_normalo = {_id:3,name:"Piet", lastname:"Doe(N)", street:"Takustraße 9",zipcode: "14195",
+        city : "Berlin",  country:"Germany",phone: 88325652, dateOfBirth:"1990-06-04",isPublic:false,owner : "normalo",
+        lat:52.45585,lng:13.29738
+    }
+    let  contacts= [contact1_admina,contact2_admina,contact1_normalo,contact2_normalo]
+
+    let main = async function  (){
+        try {
+            await connectDB()
+
+            if (await doesCollectionExistInDb()){
+                client.db(dbName).collection("contacts").drop();
+            }
+
+            await client.db(dbName).collection("contacts").insertMany(contacts );
+
+
+        } catch (e) {
+            console.log(e);
+        }
+        finally {
+            await client.close();
+
+
+        }
+
+    }
+    await main();
+}
 let admina = {username: "admina", password: "password", role:"admin"};
 let normalo = {username: "normalo", password: "password", role:"normal"};
 
 let contact_n = {
-    ID:4,
+    _id:4,
     name: "Garfield",
     lastname:"Kitty",
     street:"Badensche Str. 52",
@@ -39,7 +106,7 @@ let normalo_contact = {
 }
 
 let contact_a = {
-    ID:5,
+    _id:5,
     name: "adminas",
     lastname:"friend",
     street:"Unter den Linden 6",
@@ -77,19 +144,19 @@ let admina_contact = {
 
 
 
-let contact1_admina = { ID:0,name: "Unknown", lastname:"User(A)", street:"Wilhelminenhofstraße 75A", zipcode: "10318",
+let contact1_admina = { _id:0,name: "Unknown", lastname:"User(A)", street:"Wilhelminenhofstraße 75A", zipcode: "10318",
     city :"Berlin", country:"Germany", phone: 353637437, dateOfBirth: "1990-06-04", isPublic: true,owner : "admina",
     lat:52.49326,lng:13.52614
 }
-let contact2_admina = {ID:1,name:"John", lastname:"Doe(A)", street:"Wilhelminenhofstraße 75A", zipcode:"12459",
+let contact2_admina = {_id:1,name:"John", lastname:"Doe(A)", street:"Wilhelminenhofstraße 75A", zipcode:"12459",
     city :"Berlin",  country:"Germany",phone: 6436377, dateOfBirth:"1990-04-07", isPublic:false ,owner : "admina",
     lat:52.4581,lng:13.52709
 }
-let contact1_normalo = {ID:2,name:"Dennis",lastname: "Doe(N)",street: "Straße des 17. Juni 135", zipcode:"10623",
+let contact1_normalo = {_id:2,name:"Dennis",lastname: "Doe(N)",street: "Straße des 17. Juni 135", zipcode:"10623",
     city :"Berlin",  country:"Germany",phone: 353637437,dateOfBirth: "1990-06-04", isPublic:true,owner : "normalo",
     lat:52.51274,lng:13.3269
 }
-let contact2_normalo = {ID:3,name:"Piet", lastname:"Doe(N)", street:"Takustraße 9",zipcode: "14195",
+let contact2_normalo = {_id:3,name:"Piet", lastname:"Doe(N)", street:"Takustraße 9",zipcode: "14195",
     city : "Berlin",  country:"Germany",phone: 88325652, dateOfBirth:"1990-06-04",isPublic:false,owner : "normalo",
     lat:52.45585,lng:13.29738
 }
@@ -108,6 +175,7 @@ let  normalo_contacts_my= [contact1_normalo,contact2_normalo,contact_n]
 
 describe("Users",()=> {
     it("authenticate Admina",async () => {
+        await clearDB();
 
 
         const res = await request
@@ -274,7 +342,7 @@ describe("get contact (get) ",()=> {
 
 describe("update contact (put) ",()=> {
     it("update /Contacts, normalo",async ()=>{
-        normalo_contact["ID"] = 4
+        normalo_contact["_id"] = 4
         normalo_contact["Street & Number"] = "Luxemburger Str. 10"
         normalo_contact["ZIP"] = "13353"
 
@@ -299,7 +367,7 @@ describe("update contact (put) ",()=> {
     })
 
     it("update /Contacts, admina",async ()=>{
-        admina_contact["ID"] = 5
+        admina_contact["_id"] = 5
         admina_contact["Street & Number"] = "Am Neuen Palais 10"
         admina_contact["ZIP"] = "14469"
         admina_contact["City"] = "Potsdam"
@@ -332,7 +400,7 @@ describe("update contact (put) ",()=> {
 
 
     })
-    it("update /Contacts, unknown contact ID",async ()=>{
+    it("update /Contacts, unknown contact _id",async ()=>{
 
         await request.put("contacts/10").send(admina_contact).expect(400)
 
